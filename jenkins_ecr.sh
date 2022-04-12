c8f361c12a16936c0c92c8b44b31ba01b7de0a8b
mphelps <michaeljphelps1978@gmail.com>
2022-04-12 20:19:14 +0000

    teardown

--- jenkins_ecr.sh
+++ jenkins_ecr.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-ecs-cli configure --cluster jump-capstone --default-launch-type EC2 \
---config-name jump-capstone --region us-east-1
+# ecs-cli configure --cluster jump-capstone --default-launch-type EC2 \
+# --config-name jump-capstone --region us-east-1
 
-ecs-cli configure profile --access-key $AWS_ACCESS_KEY_ID \
---secret-key $AWS_SECRET_ACCESS_KEY \
---profile-name jump-capstone-profile
+# ecs-cli configure profile --access-key $AWS_ACCESS_KEY_ID \
+# --secret-key $AWS_SECRET_ACCESS_KEY \
+# --profile-name jump-capstone-profile
 
-ecs-cli up --keypair jump-capstone --capability-iam --size 2 --instance-type t2.micro \
---cluster-config jump-capstone --ecs-profile jump-capstone-profile --port 5000
+# ecs-cli up --keypair jump-capstone --capability-iam --size 2 --instance-type t2.micro \
+# --cluster-config jump-capstone --ecs-profile jump-capstone-profile --port 5000
 
-sleep 60
+# sleep 60
 
-ecs-cli compose up --create-log-groups --cluster-config jump-capstone \
---ecs-profile jump-capstone-profile
+# ecs-cli compose up --create-log-groups --cluster-config jump-capstone \
+# --ecs-profile jump-capstone-profile
 
-ecs-cli ps --cluster-config jump-capstone --ecs-profile jump-capstone-profile
+# ecs-cli ps --cluster-config jump-capstone --ecs-profile jump-capstone-profile
 
 # When we're ready to tear down the cluster:
 
-#ecs-cli down --force --cluster-config jump-capstone --ecs-profile jump-capstone-profile
+ecs-cli down --force --cluster-config jump-capstone --ecs-profile jump-capstone-profile
