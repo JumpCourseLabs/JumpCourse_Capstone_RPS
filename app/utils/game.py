5582335930dd3baccfb2d06ca86d94f2b653c37a
mphelps <michaeljphelps1978@gmail.com>
2022-04-12 02:33:07 +0000

    updates and bugfixes

--- app/utils/game.py
+++ app/utils/game.py
@@ -1,9 +1,12 @@
 from random import randint
-
-
 class Game:
     def __init__(self) -> None:
-        self.abilities = ['rock', 'paper', 'scissors']
+        self.abilities = [
+            'rock',
+            'paper',
+            'scissors'
+        ]
+        
         self.player_score = 0
         self.bot_score = 0
         self.bot = None
@@ -12,28 +15,27 @@ class Game:
         self.user = None
         self.started = False
         self.is_in_progress = False
-
+        
     def select(self):
-        self.bot = self.abilites[randint(0, 2)]
-
-    def set_winner(self, user_choice):
+        self.bot = self.abilities[randint(0 , 2)]
+    
+    def set_winner(self , user_choice):
         self.user = user_choice
-
-        self.selct()
-
+        
+        self.select()
+        
         self.games += 1
-
+        
         if(user_choice == self.bot):
             self.winner = 'Draw'
-
+             
         elif((user_choice == 'rock' and self.bot == 'paper') or (user_choice == 'paper' and self.bot == 'scissors') or (user_choice == 'scissors' and self.bot == 'rock')):
             self.bot_score += 1
-            self.winner = 'Bot'
-
+            self.winner = "Bot"
         else:
             self.winner = "Player"
             self.player_score += 1
             return "Player"
-
+    
     def refresh_content(self):
         self.__init__()
